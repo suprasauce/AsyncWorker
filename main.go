4bb9d2fbbdbd7c9dbfdc0439779987dc10f34aad
suprasauce <gjbhullar16@gmail.com>
2025-12-24 22:04:41 +0530

    Add a simple worker pool

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+type Job struct {
+	Id int
+	Num int
+}
+
+var jobQueue chan Job
+const JOB_QUEUE_SIZE int = 4
+const N_WORKERS int = 2
+
+
+func SpoolWorker() {
+	for {
+		j:=  <- jobQueue
+		println(j.Num)
+	}
+}
+
+func init() {
+	jobQueue = make(chan Job, JOB_QUEUE_SIZE)
+	for i := 0;i<N_WORKERS;i++ {
+		go SpoolWorker()
+	}
+}
+
+func SendJob(j Job) {
+	jobQueue <- j // currently this will block when queue is full
+}
+
+func main() {
+	SendJob(Job{Id: 1, Num: 3})
+	for {
+
+	}
+}
