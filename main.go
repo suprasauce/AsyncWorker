808297331052b56f43a06f5f4ee4cdb4a6083449
suprasauce <gjbhullar16@gmail.com>
2025-12-25 21:06:36 +0530

    Update code

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
+
 const JOB_QUEUE_SIZE int = 4
 const N_WORKERS int = 2
 
 var orch Orchestrator
-var lg logger
 
 func init() {
 	orch.Init(JOB_QUEUE_SIZE, N_WORKERS)
-	lg.Init()
 }
 
 func main() {
@@ -16,5 +15,4 @@ func main() {
 		orch.SendJob(Job{Num: i})
 	}
 	orch.GraceFulShutdown()
-	lg.GraceFulShutdown()
 }
--- orchestrator.go
+++ orchestrator.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
+	"time"
 )
 
 type Job struct {
@@ -10,27 +12,32 @@ type Job struct {
 }
 
 type Orchestrator struct {
-	jobQueue chan Job
-	wg       sync.WaitGroup
+	jobQueue   chan Job
+	wg         sync.WaitGroup
+	mCtx       context.Context
+	wCtx       context.Context
+	wCtxCancel context.CancelFunc
 }
 
 func (obj *Orchestrator) Init(jobQueueSize int, nWorkers int) {
+	obj.mCtx = context.Background()
+	obj.wCtx, obj.wCtxCancel = context.WithCancel(obj.mCtx)
 	obj.jobQueue = make(chan Job, jobQueueSize)
-	for i := 0;i<nWorkers;i++ {
-		go obj.SpoolWorker(i)
+	for i := 0; i < nWorkers; i++ {
+		go obj.SpoolWorker(obj.wCtx, i)
 	}
 }
 
-func (obj *Orchestrator) SpoolWorker(workerId int) {
-	lg.SendLog(fmt.Sprintf("Worker with Id %d spooled", workerId))
+func (obj *Orchestrator) SpoolWorker(ctx context.Context, workerId int) {
+	fmt.Printf("Worker with Id %d spooled\n", workerId)
 	obj.wg.Add(1)
 	defer func() {
 		if ok := recover(); ok != nil {
 			/* panic situaion arised, as of current implementation we will drop the current job
 			   and respawn this worker
 			*/
-			lg.SendLog(fmt.Sprintf("Panic for worker Id: %d, message: %v", workerId, ok))
-			obj.SpoolWorker(workerId)
+			fmt.Printf("Panic for worker Id: %d, message: %v\n", workerId, ok)
+			obj.SpoolWorker(ctx, workerId)
 		}
 		obj.wg.Done()
 	}()
@@ -42,31 +49,53 @@ func (obj *Orchestrator) SpoolWorker(workerId int) {
 	// 	obj.ProcessJob(j)
 	// }
 	/*
-	Below is more idiomatic. The range automatically handles if channel is closed or not
-	and exits the loop when there are no more values to read.
+		Below is more idiomatic. The range automatically handles if channel is closed or not
+		and exits the loop when there are no more values to read.
 	*/
-	for j := range obj.jobQueue {
-		obj.ProcessJob(workerId, j)
-		if j.Num == 211 {
-			pp := 0
-			a := 2/pp // here panic will occur, so worker should respool
-			println(a)
+	// for j := range obj.jobQueue {
+	// 	obj.ProcessJob(ctx, workerId, j)
+	// 	if j.Num == 211 {
+	// 		pp := 0
+	// 		a := 2 / pp // here panic will occur, so worker should respool
+	// 		println(a)
+	// 	}
+	// }
+	/*
+		Above one doesnt use context, so using the below one
+	*/
+	for {
+		select {
+		case j, ok := <-obj.jobQueue:
+			if !ok {
+				return
+			}
+			obj.ProcessJob(ctx, workerId, j)
+		case <-ctx.Done():
+			return
 		}
 	}
 }
 
 func (obj *Orchestrator) SendJob(j Job) {
-	obj.jobQueue <- j
+	obj.jobQueue <- j // this is currently blocking in nature
 }
 
-func (obj *Orchestrator) ProcessJob(wokerId int, j Job) {
-	lg.SendLog(fmt.Sprintf("Work done by worker with Id %d, value is %d", wokerId, j.Num))
+/*
+we are passing worker context here, However we can create a job context derived from
+worker context.
+*/
+func (obj *Orchestrator) ProcessJob(ctx context.Context, wokerId int, j Job) {
+	time.Sleep(time.Second*2)
+	fmt.Printf("Work done by worker with Id %d, value is %d\n", wokerId, j.Num)
 }
 
+/*
+For now cancellation policy is dropping jobs which are buffered but waiting for the current
+processing jobs to be completed.
+*/
 func (obj *Orchestrator) GraceFulShutdown() {
 	close(obj.jobQueue)
+	obj.wCtxCancel() // cancelling all the workers
 	obj.wg.Wait()
-	lg.SendLog(fmt.Sprintf("Async Worker Gracefully shutdown"))
+	fmt.Println("Async Worker Gracefully shutdown")
 }
-
-
